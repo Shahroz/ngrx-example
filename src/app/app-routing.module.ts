72eb25482e257fb0525da6acd73c2ebf5c1bdff8
Shahroz Allauddin <smshahroz.allauddin@gmail.com>
2023-01-22 20:36:05 +0500

    #time 10m Code refactored

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import {UnauthGuardService} from "@app/core/guards/unauth-guard.service";
 import {AuthGuardService} from "@app/core/guards/auth-guard.service";
 
